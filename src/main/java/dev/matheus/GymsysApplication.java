d1a758560a301c1176b89c037ca229d617f14f2d
Matheus <rmatheusluiz@gmail.com>
2026-04-30 10:54:13 -0300

    first commit

---
+++ src/main/java/dev/matheus/GymsysApplication.java
@@ -0,0 +1,13 @@
+package dev.matheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheus/GymsysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
