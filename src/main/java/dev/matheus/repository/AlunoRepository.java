09729ff72b3eeeb88d357763c3fe9aa6c0b2a2bc
Matheus <rmatheusluiz@gmail.com>
2026-05-09 22:16:48 -0300

    feat: implementa repositorio para a entidade aluno

---
+++ src/main/java/dev/matheus/repository/AlunoRepository.java
@@ -0,0 +1,7 @@
+package dev.matheus.repository;
+
+import dev.matheus.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
