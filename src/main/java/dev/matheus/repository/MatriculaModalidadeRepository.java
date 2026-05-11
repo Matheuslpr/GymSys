6f7f316ac7a1d4c93dd43fa538c9e9e7af2cdaca
Matheus <rmatheusluiz@gmail.com>
2026-05-11 16:47:34 -0300

    feat: implementa repositorio para a entidade matriculaModalidade

---
+++ src/main/java/dev/matheus/repository/MatriculaModalidadeRepository.java
@@ -0,0 +1,7 @@
+package dev.matheus.repository;
+
+import dev.matheus.model.MatriculaModalidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaModalidadeRepository extends JpaRepository<MatriculaModalidade, Long> {
+}
