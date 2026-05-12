526eb6e712811bac96254b8dbc751c66ac214f7e
Matheus <rmatheusluiz@gmail.com>
2026-05-11 21:50:49 -0300

    feat: implementa metodo existByEmail

--- src/main/java/dev/matheus/repository/AlunoRepository.java
+++ src/main/java/dev/matheus/repository/AlunoRepository.java
@@ -4,4 +4,5 @@ import dev.matheus.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    boolean existsByEmail(String email);
 }
