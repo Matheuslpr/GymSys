7e64dc264b10b2f532eb1b6f8a4a62808da1eb24
Matheus <rmatheusluiz@gmail.com>
2026-07-08 23:23:31 -0300

    feat(repository): adiciona metodos de consulta para regras de negocio e relatorios

--- src/main/java/dev/matheus/repository/MatriculaModalidadeRepository.java
+++ src/main/java/dev/matheus/repository/MatriculaModalidadeRepository.java
@@ -4,4 +4,7 @@ import dev.matheus.model.MatriculaModalidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MatriculaModalidadeRepository extends JpaRepository<MatriculaModalidade, Long> {
+
+    boolean existsByMatriculaIdAndModalidadeId(Long matriculaId, Long modalidadeId);
+    boolean existsByMatriculaIdAndModalidadeIdAndIdNot(Long matriculaId, Long modalidadeId, Long id);
 }
--- src/main/java/dev/matheus/repository/MatriculaRepository.java
+++ src/main/java/dev/matheus/repository/MatriculaRepository.java
@@ -1,7 +1,10 @@
 package dev.matheus.repository;
 
 import dev.matheus.model.Matricula;
+import dev.matheus.model.enums.StatusMatricula;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+
+    boolean existsByAlunoIdAndStatus(Long alunoId, StatusMatricula status);
 }
